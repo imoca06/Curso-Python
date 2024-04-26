ed061e7345ebb94571d57a8e36c0606913fd54b2
imoca06 <imoca06@gmail.com>
2024-04-26 23:15:45 +0200

    V 1.4

---
+++ EsNúmeroPrimo.py
@@ -0,0 +1,26 @@
+# comprobar si un número es primo
+def es_primo(n):
+    if n < 2:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+numero = 2568
+if es_primo(numero):
+    print(f"{numero} es primo")
+else:
+    print(f"{numero} no es primo")
+# devuelve: 2568 no es p
+numero = 2978
+if es_primo(numero):
+    print(f"{numero} es primo")
+else:
+    print(f"{numero} no es primo")
+# devuelve: 2978 no es primo
+
+
+
+
+
