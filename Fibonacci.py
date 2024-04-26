01ce640cff029a6d9fabad3cb1adad40ad3798da
imoca06 <imoca06@gmail.com>
2024-04-26 22:58:20 +0200

    V 1.4

--- Fibonacci.py
+++ Fibonacci.py
@@ -8,3 +8,5 @@ def fibonacci(n):
     print()
 fibonacci(1000)
 # devuelve: 0  1  1  2  3  5  8  13  21  34  55  89  144  233  377  610  987
+fibonacci(10000)
+# devuelve: 0  1  1  2  3  5  8  13  21  34  55  89  144  233  377  610  987  1597  2584  4181  6765
