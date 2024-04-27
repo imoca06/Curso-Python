2a57f3507c958b85b62102ee3a268a7c77198d69
imoca06 <imoca06@gmail.com>
2024-04-28 00:33:44 +0200

    V 1.5

---
+++ OrdenaciónDatos.py
@@ -0,0 +1,9 @@
+# ordenación para un conjunto de datos numéricos
+
+datos = [87, 41, 14, 29, 65, 96, 2, 55, 33, 78, 10, 57, 8, 93, 70, 51, 20, 39, 84, 16, 72, 49, 97, 1, 28, 90, 45, 60, 22, 53]
+def ordenar_datos(datos):
+    datos.sort()
+    return datos
+print(ordenar_datos(datos))
+# devuelve: [1, 2, 8, 10, 14, 16, 20, 22, 28, 29, 33, 39, 41, 45, 49, 51, 53, 55, 57, 60, 65, 70, 72, 78, 84, 87, 90, 93, 96, 97]
+
