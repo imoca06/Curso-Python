95196f19021b10f4c2ec41609cad8afaac3a9d5a
imoca06 <imoca06@gmail.com>
2024-05-01 05:04:11 +0200

    V 1.8 caso provisional

--- CasoPráctico.py
+++ CasoPráctico.py
@@ -17,12 +17,16 @@ class GestorTareas:
     def __init__(self):
         self.tareas = {}
 
+        # Lista de tareas
+    def lista_tareas(self, tarea):
+        self.tareas[tarea.id_tarea] = tarea
+        print(f"ID: {tarea.id_tarea}, Nombre: {tarea.nombre}") # Imprime ID: x , Nombre: xxx
+
         # Agregar tareas
 
-    def agregar_tarea(self, tarea):
+    def agregar_tarea(self, tarea):        
         self.tareas[tarea.id_tarea] = tarea
-        print(f"Tarea agregada - ID: {tarea.id_tarea}, Nombre: {tarea.nombre}")
-        # Imprime Tarea agregada - ID: x , Nombre: xxx
+        print(f"ID: {tarea.id_tarea}, Nombre: {tarea.nombre}") # Imprime ID: x , Nombre: xxx
 
         # Marcar tarea como completada
 
@@ -34,8 +38,7 @@ class GestorTareas:
             print("No se encontró la tarea con ese ID.") # Imprime No se encontró la tarea con ese ID.
 
         # Mostrar todas las tareas
-    def mostrar_tareas(self):
-            print("Tareas:") # Imprime Tareas:
+    def mostrar_tareas(self):            
             for id_tarea, tarea in self.tareas.items():
                 print(tarea) # Imprime tarea
           
@@ -48,30 +51,69 @@ class GestorTareas:
         else:
             print("No se encontró la tarea con ese ID.") # Imprime No se encontró la tarea con ese ID.
 
+        # Buscar tareas borradas
+
+    def buscar_tareas_borradas(self):
+              for id_tarea, tarea in self.tareas.items():
+                print(tarea) # Imprime tarea
+
+
     
 
 # Ejemplo de uso
 
 if __name__ == "__main__":
     gestor = GestorTareas()
-    
-    # Agregar tareas
-    print("Tareas agregadas:")
+
+    # Lista de tareas
+    print("\nLista de tareas:") # Imprime Lista de tareas:
     tarea1 = Tarea(1, "Pasear")
     tarea2 = Tarea(2, "Comprar pan")
-    tarea3 = Tarea(3, "Tomar aperitivo")
-    tarea4 = Tarea(4, "Comer")
-    tarea5 = Tarea(5, "Recoger")
-    tarea6 = Tarea(6, "Jugar coches")
-    tarea7 = Tarea(7, "Parquear")
+    tarea3 = Tarea(3, "Comer")
+    tarea4 = Tarea(4, "Recoger")
+    tarea5 = Tarea(5, "Jugar coches")
+    tarea6 = Tarea(6, "Parquear")
+    tarea7 = Tarea(7, "Duchar")
+    tarea8 = Tarea(8, "Cenar")
+    tarea9 = Tarea(9, "Jugar")
+    tarea10 = Tarea(10, "Dormir")
+
+    
+    gestor.lista_tareas(tarea1)
+    gestor.lista_tareas(tarea2)
+    gestor.lista_tareas(tarea3)
+    gestor.lista_tareas(tarea4)
+    gestor.lista_tareas(tarea5)
+    gestor.lista_tareas(tarea6)
+    gestor.lista_tareas(tarea7)
+    gestor.lista_tareas(tarea8)
+    gestor.lista_tareas(tarea9)
+    gestor.lista_tareas(tarea10)
+
+
+
+
+
+    # Agregar tareas
+
+    print("\nTareas agregadas:") # Imprime Tareas agregadas:
+    tarea1 = Tarea(1, "Pasear")
+    tarea2 = Tarea(2, "Comprar pan")
+    tarea3 = Tarea(3, "Comer")
+    tarea4 = Tarea(4, "Recoger")
+    tarea5 = Tarea(5, "Jugar coches")
+    tarea6 = Tarea(6, "Parquear")
+    tarea7 = Tarea(7, "Duchar")
+    tarea8 = Tarea(8, "Cenar")
+    tarea9 = Tarea(9, "Jugar")
+    tarea10 = Tarea(10, "Dormir")
+    
    
-    gestor.agregar_tarea(tarea1)
-    gestor.agregar_tarea(tarea2)
-    gestor.agregar_tarea(tarea3)
-    gestor.agregar_tarea(tarea4)
-    gestor.agregar_tarea(tarea5)
-    gestor.agregar_tarea(tarea6)
-    gestor.agregar_tarea(tarea7)
+    gestor.agregar_tarea(tarea8)
+    gestor.agregar_tarea(tarea9)
+    gestor.agregar_tarea(tarea10)
+
+    
 
     # Marcar tarea como completada
 
@@ -88,13 +130,16 @@ if __name__ == "__main__":
 
     print("\nTareas borradas:") # Imprime Tareas borradas:
     gestor.borrar_tarea(1)
-    gestor.borrar_tarea(3)
-    gestor.borrar_tarea(5)
+    gestor.borrar_tarea(2)
+   
 
     #buscar tarea borrada
-    print("\nTodas las tareas:") # Imprime Todas las tareas:
-    gestor.mostrar_tareas()
+    print("\nTodas las tareas pendientes:") # Imprime Todas las tareas pendientes:
+    gestor.buscar_tareas_borradas()
+
+
   
+   
     
 
 
